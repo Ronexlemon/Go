7ba0f8556cfb6310ae2d7b21b8ca4e6b8cd7898e
RonexLemon <ronexondimu@gmail.com>
2023-12-29 07:59:59 +0300

    values

---
+++ week-one/values/values.go
@@ -0,0 +1,12 @@
+package main
+
+import ("fmt")
+
+func main(){
+	fmt.Println("go"+"lang") //strings
+	fmt.Println(true && false) //boolean
+	fmt.Println(7.0 +7.33) //float
+	fmt.Println(5+5) // integer
+	
+
+}
