e529ce70485c5d84e22fa552657310fd390f001e
RonexLemon <ronexondimu@gmail.com>
2023-12-29 08:32:26 +0300

    varibales

---
+++ week-one/one/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("go" + "lang") // strings
+	fmt.Println(true && false) // boolean
+	fmt.Println(7.0 + 7.33)    // float
+	fmt.Println(5 + 5)         // integer
+
+	// Call the Variable function from the same package
+	 Variable()
+	
+}
