714990d44075a98d0b1e1bb1876f81a74f810485
RonexLemon <ronexondimu@gmail.com>
2024-01-11 09:59:16 +0300

    values

--- golearn/one/stringFormat.go
+++ golearn/one/stringFormat.go
@@ -5,12 +5,17 @@ import (
 	"os"
 )
 
+var ff = fmt.Printf
 type point struct{
 	x,y int
 }
 func StringFormating() {
 	p:=point{1,2}
 
-	fmt.Printf("struct1: %v\n",p)
+	ff("struct1: %v\n",p) //%v prints the struct values
+	ff("struct1: %+v\n",p) //+%includes struct values
+	ff("struct1: %#v\n",p) //%v prints the syNTAX
+	ff("struct1: %T\n",p) //%v type of the value
+	
 	fmt.Fprintf(os.Stderr, "io: an %s\n", "error")
 }
