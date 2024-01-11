ecf9dfaebbf5a854a49c9d59511f6c64b4a01e34
RonexLemon <ronexondimu@gmail.com>
2024-01-11 10:07:47 +0300

    fomart:floats

--- golearn/one/stringFormat.go
+++ golearn/one/stringFormat.go
@@ -19,6 +19,11 @@ func StringFormating() {
 	ff("bool: %t\n",true) //%v fomrating boolens
 	ff("int: %d\n",123)
 	ff("bin %b\n",14)//binary representation
+	ff("char: %c\n",33) //character representation of a number
+	ff("hex: %x\n",433) //hex encoding
+	ff("float1: %f\n",70.6) //floating point
+	ff("float2: %e\n",124000000.0) //scientific notation
+	ff("float2: %E\n",124000000.0)
 	
 	fmt.Fprintf(os.Stderr, "io: an %s\n", "error")
 }
